c72f55f592c514bd04b1fd213742b526217a31c3
Henrik <henrik@eideberg.se>
2024-01-12 13:16:50 +0100

    Delete __tests__/sum.test.js

--- __tests__/sum.test.js
+++
@@ -1,6 +0,0 @@
-// https://www.stanleyulili.com/node/node-modules-import-and-use-functions-from-another-file
-const sum = require('./../sum');
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
